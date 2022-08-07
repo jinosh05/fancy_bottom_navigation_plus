5e685fafc586ab94e49d61361d242b9f574cac79
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-08-07 18:49:17 +0530

    HalfPainter created to paint ARC

---
+++ lib/components/half_painter.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart';
+
+class HalfPainter extends CustomPainter {
+  HalfPainter(Color paintColor, this.height, {this.outline = 10}) {
+    arcPaint = Paint()..color = paintColor;
+  }
+
+  late Paint arcPaint;
+  final double? height;
+  final double outline;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect beforeRect =
+        Rect.fromLTWH(0, (size.height / 2) - outline, outline, outline);
+    final Rect largeRect = Rect.fromLTWH(
+      outline,
+      0,
+      size.width - outline * 2,
+      height ?? 70,
+    );
+    final Rect afterRect = Rect.fromLTWH(
+        size.width - outline, (size.height / 2) - outline, outline, outline);
+
+    final path = Path();
+    path.arcTo(beforeRect, radians(0), radians(90), false);
+    path.lineTo(outline * 2, size.height / 2);
+    path.arcTo(largeRect, radians(0), -radians(180), false);
+    path.moveTo(size.width - outline, size.height / 2);
+    path.lineTo(size.width - outline, (size.height / 2) - outline);
+    path.arcTo(afterRect, radians(180), radians(-90), false);
+    path.close();
+
+    canvas.drawPath(path, arcPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
