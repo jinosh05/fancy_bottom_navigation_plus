0f81262f857ad24e8a4e5c8277014e10bd26685e
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-08-07 17:27:36 +0530

    TabItem base created

---
+++ lib/components/tab_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TabItem extends StatelessWidget {
+  const TabItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Expanded(child: Stack());
+  }
+}
